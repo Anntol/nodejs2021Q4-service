d1815da3127651aa5a379fd58d4d883ecadb8e1c
Anna Tolstova <anna_tolstova@hotmail.com>
2021-12-19 22:28:58 +0200

    fix: restore swagger

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,11 @@
 import fastify, { FastifyReply, FastifyRequest } from "fastify";
-
-// import swaggerUI from 'fastify-swagger';
+import swagger, { SwaggerOptions } from "fastify-swagger";
 
 import boardRouter from './resources/boards/board.router.js';
 import taskRouter from './resources/tasks/task.router.js';
 import userRouter from './resources/users/user.router.js';
 
 const app = fastify();
-// TODO: restore swagger
-/* app.register(swaggerUI, {
-  exposeRoute: true,
-  routePrefix: '/doc',
-  swagger: {
-    info: { title: 'RS NodeJsQ4 Trello' },
-  },
-}); */ 
 
 app.get('/', (_:FastifyRequest, reply:FastifyReply) => {
   reply.send('Service is running!');
@@ -23,5 +14,16 @@ app.get('/', (_:FastifyRequest, reply:FastifyReply) => {
 app.register(userRouter, { prefix: '/users' });
 app.register(boardRouter, { prefix: '/boards' });
 app.register(taskRouter, { prefix: 'boards/:boardId/tasks' });
+
+const swaggerOpts: SwaggerOptions = {
+  exposeRoute: true,
+  routePrefix: '/doc',
+  mode: 'static',
+  specification: {
+    path: './doc/api.yaml',
+    baseDir: ''
+  }
+};
+app.register(swagger, swaggerOpts);
 
 export default app;
