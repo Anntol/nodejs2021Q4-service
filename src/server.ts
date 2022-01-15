7db12de048f918b701f888f77ec2d3ee241cf2dc
Anna Tolstova <anna_tolstova@hotmail.com>
2022-01-16 01:14:13 +0200

    init: create typeorm postgres connection

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
+import "reflect-metadata";
+import pkg from "typeorm";
+
 import app from './app.js';
 import config from './common/config.js';
 
-const start = async () => {
-  const port = config.PORT ?? 4000;
-  app.listen(port, '0.0.0.0', (err, address) => {
-    if (err) {
-      app.log.error(err);
-      process.exit(1);
-    }
-    app.log.info(`server listening on ${address}`);
-  });
-}
+const { createConnection } = pkg;
+createConnection().then(async (connection) => {
+  const start = async () => {
+    const port = config.PORT ?? 4000;
+    app.listen(port, '0.0.0.0', (err, address) => {
+      if (err) {
+        app.log.error(err);
+        process.exit(1);
+      }
+      app.log.info(`server listening on ${address}`);
+      app.log.info(`database connected: ${connection.isConnected}`);
+    });
+  }
+  start();
+}).catch ((error) => app.log.error(error));
 
-start();
